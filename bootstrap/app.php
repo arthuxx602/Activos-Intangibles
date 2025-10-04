0b3d3e7a4d4796d94c70026b20f90d6553afba4c
arthuxx602 <arthuxx602@gmail.com>
2025-10-04 07:52:36 -0500

    cambios en la api en administrador

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,10 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php', // 👈 AGREGAR ESTA LÍNEA // puesto que ya es de laravel 12, toca agregarlo manualmente.
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+    ->withMiddleware()
+    ->withExceptions()
+    ->create();
